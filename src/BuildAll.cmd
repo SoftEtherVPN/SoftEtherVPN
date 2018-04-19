7b98a799c89667a53ae2a8074979e78fb3f5dd40
Davide Beatrici <davidebeatrici@gmail.com>
2018-04-19 20:43:31 +0200

    BuildAll.cmd: correctly start BuildUtil.exe, check if required software exists and exit in case of error

--- src/BuildAll.cmd
+++ src/BuildAll.cmd
@@ -4,16 +4,32 @@ SET BATCH_DIR_NAME=%0\..
 SET NOW_TMP=%time:~0,2%
 SET NOW=%date:~0,4%%date:~5,2%%date:~8,2%_%NOW_TMP: =0%%time:~3,2%%time:~6,2%
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+if exist "C:\Program Files\Microsoft Visual Studio 9.0" (
+	call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+) else if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+) else (
+	echo "Visual Studio 2008 not found!"
+	exit /b 1
+)
+
+if not exist "C:\windows\Microsoft.NET\Framework\v3.5" (
+	echo ".NET Framework 3.5 not found!"
+	exit /b 1
+)
+
 echo on
 
-del %BATCH_DIR_NAME%\bin\BuildUtil.exe
+if exist "%BATCH_DIR_NAME%\bin\BuildUtil.exe" (
+	del "%BATCH_DIR_NAME%\bin\BuildUtil.exe"
+)
 
 C:\windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /toolsversion:3.5 /verbosity:detailed /target:Clean /property:Configuration=Debug "%BATCH_DIR_NAME%\BuildUtil\BuildUtil.csproj"
 
 C:\windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /toolsversion:3.5 /verbosity:detailed /target:Rebuild /property:Configuration=Debug "%BATCH_DIR_NAME%\BuildUtil\BuildUtil.csproj"
 
-cmd /k "%BATCH_DIR_NAME%\bin\BuildUtil.exe /CMD:All"
+cd %BATCH_DIR_NAME%\bin
 
+BuildUtil.exe /CMD:All
 
+if errorlevel 1 exit /b %errorlevel%
