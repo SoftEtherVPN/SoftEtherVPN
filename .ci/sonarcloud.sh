3c2f5a41bacc7f8b0611d39128279d8e3e0c2730
Davide Beatrici <git@davidebeatrici.dev>
2020-07-20 20:33:36 +0200

    Use "build" folder instead of "tmp" in CI scripts

--- .ci/sonarcloud.sh
+++ .ci/sonarcloud.sh
@@ -5,7 +5,7 @@ RUN_SONARCLOUD="${RUN_SONARCLOUD:-0}"
 
 if [ "${RUN_SONARCLOUD}" = "1" ] && [ ! -z ${SONAR_TOKEN+x} ]; then
    ./configure
-   build-wrapper-linux-x86-64 --out-dir bw-output make -C tmp
+   build-wrapper-linux-x86-64 --out-dir bw-output make -C build
    sonar-scanner -Dsonar.projectKey=SoftEtherVPN_SoftEtherVPN -Dsonar.organization=softethervpn -Dsonar.sources=. -Dsonar.cfamily.build-wrapper-output=bw-output -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=${SONAR_TOKEN}
 else
     echo "Skipping sonar-scan because \$RUN_SONARCLOUD != \"1\" or \$SONAR_TOKEN is not set"
