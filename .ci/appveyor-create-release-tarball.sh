544f03ec8bbfac9903f75d6d12aa5e5fce3a4d25
Ilya Shipitsin <chipitsine@gmail.com>
2023-04-21 22:26:30 +0200

    CI: drop AppVeyor source release creation in favour of GH Actions

--- .ci/appveyor-create-release-tarball.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-tar --exclude=.git --transform "s//SoftEtherVPN-${APPVEYOR_REPO_TAG_NAME}\//" -czf /tmp/softether-vpn-src-${APPVEYOR_REPO_TAG_NAME}.tar.gz . 
-appveyor PushArtifact /tmp/softether-vpn-src-${APPVEYOR_REPO_TAG_NAME}.tar.gz
-
