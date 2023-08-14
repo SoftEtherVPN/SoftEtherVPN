a80d3f20321a9de15cd3c6a9baa1a39782c4b528
barracuda156 <vital.had@gmail.com>
2023-08-14 15:54:56 +0800

    TunTap.h: fix for undefined u_char, u_short on MacOS

--- src/Mayaqua/TunTap.h
+++ src/Mayaqua/TunTap.h
@@ -162,6 +162,13 @@ struct tundladdr {
 #ifndef _NET_IF_TUN_H_
 #define _NET_IF_TUN_H_
 
+#ifdef UNIX_MACOS
+#ifndef _DARWIN_C_SOURCE
+#define _DARWIN_C_SOURCE
+#endif
+#include <sys/types.h>
+#endif
+
 /* Refer to if_tunvar.h for the softc stuff */
 
 /* Maximum transmit packet size (default) */
