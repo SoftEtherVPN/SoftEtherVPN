961b2dc1d92680dcddfc32bcf466d73b353c30d9
TheKeyblader <jef97310@gmail.com>
2026-02-18 23:40:22 +0100

    create hub page initial layout

---
+++ src/bin/hamcore/wwwroot/svelte-admin/src/lib/components/form/form-label.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Label } from 'formsnap';
+	import type { ComponentProps } from 'svelte';
+	import { FieldLabel } from '../ui/field';
+
+	let { children: childrenProp, ...restProps }: ComponentProps<typeof Label> = $props();
+</script>
+
+<Label {...restProps}>
+	{#snippet child({ props })}
+		<FieldLabel {...props}>
+			{@render childrenProp?.()}
+		</FieldLabel>
+	{/snippet}
+</Label>
--- src/bin/hamcore/wwwroot/svelte-admin/src/lib/components/ui/form/form-button.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Button, type ButtonProps } from "$lib/components/ui/button/index.js";
-
-	let { ref = $bindable(null), ...restProps }: ButtonProps = $props();
-</script>
-
-<Button bind:ref type="submit" {...restProps} />
